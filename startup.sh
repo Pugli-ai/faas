4f861d5b6717f6151dd7081cf2ed48081d0a80f6
Faruqt <faruqabdulsalam@yahoo.com>
2025-03-05 17:54:17 +0100

    update: add local posgres db container

--- startup.sh
+++ startup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 # handle database migrations
+echo "Generating database migrations..."
 python manage.py makemigrations
 
+# apply migrations
+echo "Applying database migrations..."
 python manage.py migrate
 
 # # create superuser if not exists
 # python scripts/create_admin.py
 
 # start the server
-exec gunicorn --bind 0.0.0.0:8000 ideapools.wsgi:application --timeout 200 --worker-class=gevent --worker-connections=1000 --workers=2 --access-logfile -
+echo "Starting server..."
+exec gunicorn --bind 0.0.0.0:8000 ideapools.wsgi:application --timeout 200 --worker-connections=1000 --workers=5 --access-logfile -
